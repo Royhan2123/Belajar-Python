bf3331fd70e06a8dc6440922a7cd1bd5b2e7a688
Royhan <royhankeren12@gmail.com>
2024-04-12 17:15:28 +0700

    boolean

---
+++ src/boolean.py
@@ -0,0 +1,10 @@
+# disini kita akan membuat tipe data boolean
+# sama seperti bahasa pemograman lainnya yaitu menggunakan True & False
+# yang membedakan hanyalah menggunakan huruf kapital
+# seperti contoh berikut :
+
+a = True # <= di bahasa pemograman python kita harus membuat true nya itu depannya kapital True
+b = False
+
+print(a)
+print(b)
