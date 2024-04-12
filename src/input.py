ad6846c882e4ca8547222d8bcc62ee01065821f2
Royhan <royhankeren12@gmail.com>
2024-04-12 17:04:44 +0700

    input

---
+++ src/input.py
@@ -0,0 +1,15 @@
+# jadi disini kita akan mencoba menginputkan data di dalam debugging atau terminal
+# contohnya seperti berikut :
+
+print("Silhakan masukkan nama anda : ")
+nama = input()
+print("")
+print("Silhakan masukkan alamat anda : ")
+alamat = input()
+print("")
+print("Silhakan masukkan jurusan anda : ")
+jurusan = input()
+
+
+
+print(f"Hello {nama}, Welcome to phycarm\nAddres {alamat}\nJurusan Anda {jurusan}")
