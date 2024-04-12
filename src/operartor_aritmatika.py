f29c9b9cf9efe47d3ccc82f444b15b91d83a136b
Royhan <royhankeren12@gmail.com>
2024-04-12 16:32:23 +0700

    operator_aritmatika

---
+++ src/operartor_aritmatika.py
@@ -0,0 +1,21 @@
+# disini saya akan membuat operator aritmatika di dalam phyton
+# sama sesperti bahasa pemograman lainnya ada menggunakan +,-,*,/,%
+# seperti contoh di bawah ini :
+
+a = 10
+b = 10
+
+# Pertambahan
+print(a + b)
+
+# Perkurangan
+print(a - b)
+
+# Perkalian
+print(a * b)
+
+# Pembagian
+print(a / b)
+
+# Sisa Bagi
+print(a % b)
