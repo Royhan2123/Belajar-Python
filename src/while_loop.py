f018a62b770ce1bbc2caf9fd2f67b5e069ca667c
Royhan <royhankeren12@gmail.com>
2024-04-13 22:15:19 +0700

    while_loop

---
+++ src/while_loop.py
@@ -0,0 +1,9 @@
+# jadi disini kita akan membahas belajar tentang while loop
+# seperti contoh berikut :
+
+data = "" # <= jadi disini kita membuat sebuah variabel data dengan string kosong
+
+while data != "x": # <= lalu kita akan melaukan perulangan, ketika data tidak sama dengan x
+    print("Masukkan perulangan") # jika dia false maka dia akan membuat sebuah perulangan tanpa henti
+    # sampai benar benar mendapatkan nilai x
+    data = input("data : ") # <= jika dia true maka dia akan berhenti.
