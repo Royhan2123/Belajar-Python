57266811d02c8801c856586e216f4399abf42f25
Royhan <royhankeren12@gmail.com>
2024-04-13 20:55:34 +0700

    tipe data range

---
+++ src/tipe_data_range.py
@@ -0,0 +1,16 @@
+# jadi disini kita akan membuat sebuah tipe data range, sama seperti bahasa pemgoraman lainnya
+# dari 1..100 or 0 until 100, tetapi dalam python kita harus menggunakan (,)
+# seperti contoh berikut :
+
+nomorUrut = range(1, 10) # <= ini akan menghitung dari angka 1 sampai dengan 10
+
+for i in nomorUrut: # <= lalu kita melakukan sebuah perulangan, dan akan menghasilkan dari angka 1 - 10.
+    print(i)
+
+print("")
+
+
+# atau bisa langsung seperti ini :
+for a in range(10, 20):
+    print(a)
+
