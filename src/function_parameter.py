f24c317b09da9f6f0b5e70b71f2dc8fd7cae5c19
Royhan <royhankeren12@gmail.com>
2024-07-03 23:31:24 +0700

    function parameter

---
+++ src/function_parameter.py
@@ -0,0 +1,17 @@
+def InputYourName(name):
+    print(f"Hello {name}")
+
+InputYourName("Royhan")
+
+def name(first, last):
+    print(f"Hello {first} {last}")
+
+name("Roy", "Han")
+
+def mahasiswa(name,umur,jurusan,npm):
+    print(f"Nama saya: {name}\nUmur: {umur}\nJurusan: {jurusan}\nNPM: {npm}")
+
+
+print("*****===========================*****")
+mahasiswa("Royhan",22,"Teknik Informratika", 21110156)
+print("******==========================*******")
