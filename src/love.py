31372ce7ca0bb889cc4b21cf3c9301c5ecd9c762
Royhan <royhankeren12@gmail.com>
2024-07-17 00:26:00 +0700

    love.py

--- src/love.py
+++ src/love.py
@@ -1,16 +1,15 @@
 import turtle
 
-# Membuat layar turtle
 screen = turtle.Screen()
 screen.bgcolor("white")
 
-# Membuat turtle
-t = turtle.Turtle()
-t.shape("turtle")  # Bentuk turtle bisa diubah sesuai keinginan
-t.color("red")     # Warna turtle diatur menjadi merah
-t.speed(1)         # Kecepatan turtle (1 adalah lambat, bisa disesuaikan)
 
-# Menggambar love shape
+t = turtle.Turtle()
+t.shape("turtle")  
+t.color("red")     
+t.speed(1)         
+
+
 t.begin_fill()
 t.left(50)
 t.forward(133)
@@ -20,14 +19,14 @@ t.circle(50, 200)
 t.forward(133)
 t.end_fill()
 
-# Posisi turtle untuk menulis kata-kata di bawah love shape
-t.penup()          # Mengangkat pena turtle
-t.goto(-50, -200)  # Posisi turtle
-t.pendown()        # Menurunkan pena turtle
 
-# Menulis kata-kata
-t.color("black")   # Warna tulisan hitam
+t.penup()          
+t.goto(-50, -200)  
+t.pendown()        
+
+
+t.color("black")   
 t.write("Ini untukmu", align="center", font=("Arial", 24, "bold"))
 
-# Menahan jendela sampai ditutup pengguna
+
 turtle.done()
