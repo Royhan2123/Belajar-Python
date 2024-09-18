c5ccde93f2d9be3001acdb161043379527b86cbd
Royhan <royhankeren12@gmail.com>
2024-09-19 00:00:26 +0700

    add Decision Tree Regression

--- Face Detection/index.py
+++ Face Detection/index.py
@@ -31,7 +31,7 @@ def detect_bounding_box(vid):
     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
     for (x, y, w, h) in faces:
-        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, ), 4)0
     return faces
 
 
